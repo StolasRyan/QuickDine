2cabd89b6351f5d4aa008ec5a2bf09d9abb01602
StolasRyan <paul.pbronx@gmail.com>
2026-07-28 19:45:05 +0300

    fix for cors

--- server/server.ts
+++ server/server.ts
@@ -13,7 +13,7 @@ const app = express();
 await connectDB();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin:  process.env.CLIENT_URL,
   credentials: true
 }));
 app.use(express.json());
